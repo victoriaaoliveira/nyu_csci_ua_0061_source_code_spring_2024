169af9ba74c8cac47f7e43e0a06fb6a45e8cdb7a
Victoria Oliveira <104442459+victoriaaoliveira@users.noreply.github.com>
2024-02-20 15:30:00 -0500

    Update 04_ProgrammingChallenge.js

--- days_07_08/01-DOM_classes_selecting_multiple_elements_and_creating_new_elements/04_ProgrammingChallenge.js
+++ days_07_08/01-DOM_classes_selecting_multiple_elements_and_creating_new_elements/04_ProgrammingChallenge.js
@@ -13,13 +13,49 @@ const contentGlazed = document.getElementById('contentGlazed');
 //  event handler to make our Welcome Tab active
 tabWelcome.onclick = function() {
   // first, make this tab our 'active' tab by applying the 'active' class to it
+  tabWelcome.classList.add("active");
 
   // remove the 'active' class from the other tabs
+  tabTraditional.classList.remove("active");
+  tabGlazed.classList.remove("active");
 
   // show the content associated with this tab
+  contentWelcome.classList.remove("hidden");
 
   // hide the other two tabs
+  contentTraditional.classList.add("hidden");
+  contentGlazed.classList.add("hidden");
+}
 
+tabTraditional.onclick = function () {
+  deactiveAllTabs();
+  tabTraditional.classList.add("active");
+
+  hideAllContent();
+  contentTraditional.classList.add("active");
+
+}
+
+tabGlazed.onclick = function () {
+  deactiveAllTabs();
+  tabGlazed.classList.add("active");
+  hideAllContent();
+  contentGlazed.classList.add("active");
+
+}
+
+function deactiveAllTabs() {
+  let allTabs = document.querySelectorAll(".tab");
+  for(let i = 0; i < allTabs.length; i++){
+    allTabs[i].classList.remove("active");
+  }
+}
+
+function hideAllContent() {
+  let allContent = document.querySelectorAll(".content");
+  for(let i = 0; i < allTabs.length; i++){
+    allContent[i].classList.remove("active");
+  }
 }
 
 
